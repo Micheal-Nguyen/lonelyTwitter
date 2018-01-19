b9b0d2492803a82518cda8d4f6e3f01fd0ba11e7
Micheal Nguyen <mlnguyen@cs.ualberta.ca>
2018-01-18 21:04:01 -0700

    Lab2 Done

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,5 +1,6 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -10,16 +11,18 @@ public abstract class Tweet implements Tweetable {
 
     private String message;
     private Date date;
-
+    private ArrayList<currentMood> moods;
 
     public Tweet(String message) {
         this.message = message;
         this.date = new Date();
+        this.moods = new ArrayList<currentMood>();
     }
 
     public Tweet (String message, Date date) {
         this.message = message;
         this.date = date;
+        this.moods = new ArrayList<currentMood>();
     }
 
     public Date getDate() {
